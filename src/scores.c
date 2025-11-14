20e528fb6be24319fbc78df7be648514af2bad3a
Angelica Aguilar <isaj44631@gmail.com>
2025-11-13 21:24:14 -0600

    Accede a puntuaciones y reordena y reescribe si debe despues de cada partida

--- src/scores.c
+++ src/scores.c
@@ -13,4 +13,35 @@ void guardarPuntaje(int p) {
 	fclose(f);
 }
 
+void obtenerTop5(int top[5], int *cantidad) {
+	int lista[200];
+	int n = 0;
 
+	FILE *f = fopen("puntuaciones.txt", "r");
+	if (f == NULL) {
+		*cantidad = 0;
+		return;
+	}
+
+	while (fscanf(f, "%d", &lista[n]) == 1) {
+		n++;
+	}
+
+	fclose(f);
+
+	for (int i = 0l i < n - 1; i++) { //ordenar por BubbleSort
+		for (int j = i + 1; j < n; j++) {
+			if (lista[j] > lista[i]) {
+				int aux = lista[i];
+				lista[i] = lista[j];
+				lista[j] = aux;
+			}
+		}
+	}
+
+	*cantidad =  (n < 5 ? n : 5);
+
+	for (int i = 0; i < *cantidad; i++) {
+		top[i] = lista[i];
+	}
+}
