87b23fe70f9682a628154063acf21d0dc8627f8b
Angelica Aguilar <isaj44631@gmail.com>
2025-11-23 10:32:26 -0600

    Archivo ui.c, permite reiniciar con enter

--- src/ui.c
+++ src/ui.c
@@ -58,4 +58,19 @@ void ui_run(estado *e) {
 			if (IsKeyPressed(KEY_DOWN)) mover (e, 'S');
 		}
 
+		if (e->ganador || e-> perdido) { //una vez se gana o pierde se puede reiniciar con Enter
+			if (IsKyPressed(KEY_ENTER)) {
+				guardarPuntaje(e->puntos);
+				TableroVacio(e)
+			}
+		}
+
+		BeginDrawing();
+		ClearBackground(fondo);
+
+		dibujarTablero(e);
+		dibujarPanel(e);
+
+
+
 
