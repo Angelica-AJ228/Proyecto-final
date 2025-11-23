50ff2e8faf3b54b67399b6fce756a7df68380c27
Angelica Aguilar <isaj44631@gmail.com>
2025-11-23 15:48:18 -0600

    Agrega una funcion mas para un cambio en el ui.c para que se pueda expandir la ventana, pero todo compila bien

--- include/ui.h
+++ include/ui.h
@@ -5,7 +5,8 @@
 
 //función principal de Raylib
 void ui_run(estado *e);
-void dibujarPanel(estado *e);
+void dibujarPanel(estado *e, int panelx, int panelY);
+void dibujarTablero(estado *e, int inicioX, int inicioY, int tam, int espacio);
 
 #endif
 
